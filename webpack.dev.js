10f16d0a10c83e2e519ce16f4332468e24a7c939
Steve Montgomery <steve@chums.com>
2023-06-01 16:34:47 -0600

    chore: npm updates

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,7 +1,7 @@
 const {merge} = require('webpack-merge');
 const webpack = require('webpack');
 const common = require('./webpack.common.js');
-const path = require('path');
+const path = require('node:path');
 
 const localProxy = {
     target: {
