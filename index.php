25f849a71f2384fc9762f70cad3c5bd9164dc4e9
Steve Montgomery <steve@chums.com>
2026-01-26 16:13:18 -0700

    chore: update to WebUI2

--- index.php
+++ index.php
@@ -7,17 +7,19 @@
  * @author Steve Montgomery
  * @copyright Copyright &copy; 2013, steve
  */
+use chums\ui\WebUI2;
+use chums\user\Groups;
+use chums\ui\JSOptions;
+use chums\ui\CSSOptions;
+
 
 require_once ("autoload.inc.php");
-require_once ('access.inc.php');
 
-$bodyPath = "/apps/sage-tables";
-$title = "Sage 100 Tables";
-$description = "";
-
-$ui = new WebUI($bodyPath, $title, $description, true, 5);
-$ui->version = "2019-05-29";
-$ui->bodyClassName = 'container-fluid';
-$ui->AddCSS("public/styles.css", false, true);
-$ui->addManifest('public/js/manifest.json');
-$ui->Send();
+$ui = new WebUI2([
+    'title' => 'Sage 100 Tables',
+    'bodyClassName' => 'container-fluid',
+    'requiredRoles' => [Groups::ADMIN],
+]);
+$ui->addCSS('public/css/styles.css', CSSOptions::parse(['useTimestampVersion' => true]))
+    ->addManifestJSON('public/js/manifest.json')
+    ->render();
